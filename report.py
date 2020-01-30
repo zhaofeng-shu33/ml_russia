e2db1336d88c5b87e7cd7fedf433da96fc586700
zhaofeng-shu33 <616545598@qq.com>
2020-01-30 23:01:35 +0300

    finish simple preprocessing

--- report.py
+++ report.py
@@ -9,9 +9,27 @@ def _normalize(df, column_name):
     min_max_scaler = MinMaxScaler()
     df[column_name] = min_max_scaler.fit_transform(df[[column_name]])
 
+def _remove_abnormal(df, i):
+    # to do added
+    return
+
+def _get_data_and_label(df):
+    Y = df['price'].values
+    df.drop('price', axis=1)
+    df.drop(df.columns[0], axis=1)
+    X = df.values
+    return (X, Y)
+
 def preprocessing(df, K=4):
     '''
+        Parameters
+        ----------
+        df: Pandas dataframe
         K: number of bins used to discretize lastownage
+
+        Returns
+        X: array-like object, number of samples X number of features
+        Y: predicted values
     '''
     # convert columns with category data with right type
     categorical_class = ['brick', 'metro', 'floor',
@@ -27,13 +45,18 @@ def preprocessing(df, K=4):
         'livesp', 'totsp']
     for i in continuous_class:
         _normalize(df, i)
+    _normalize(df, 'price')
     # assume each continuous features conforms to standard normal
     # distribution, treat observations with feature larger than 3
     # sigma as abnormal and remove them in later computation
+    for i in continuous_class:
+        _remove_abnormal(df, i)
+    # finally, we return the numpy array as data, price as label
+    return _get_data_and_label(df)
 
 def main():
     df = read_raw_data()
-    preprocessing(df)
+    X, Y = preprocessing(df)
 
 if __name__ == '__main__':
     main()
